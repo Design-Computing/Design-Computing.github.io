e6a4375edc52b9e1640fb658f2a52d88124b5e28
Ben <ben@notionparallax.co.uk>
2021-05-26 17:34:11 +1000

    be explicit about how to sort data

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
   componentDidMount() {
     const api = "https://api.github.com";
     const org = "design-computing";
-    let url = `${api}/repos/${org}/me/forks?per_page=100`;
+    let url = `${api}/repos/${org}/me/forks?per_page=50&sort=newest`;
     fetch(url, {
       method: "GET",
       headers: {
