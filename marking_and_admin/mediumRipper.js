024cc8c159bc613d7d1f8edbb797ca5d21e99ade
Ben <ben@notionparallax.co.uk>
2021-06-10 14:06:55 +1000

    cleaning out old files

--- marking_and_admin/mediumRipper.js
+++
@@ -1,9 +0,0 @@
-a = document.querySelectorAll("[data-username]")
-b = [];
-
-function pullUserName(x){
-  b.push( x.getAttribute("data-username"));
-}
-
-a.forEach(pullUserName) //I don't like this, I'd rather use a map
-b.forEach(function(x){console.log(x)})
