45a8aa4eede36fb904165f5ae1270c015d3a922b
Ben Doherty <ben@notionparallax.co.uk>
2023-05-30 14:46:47 +1000

    update for 2023

--- marking_and_admin/marking_puller.py
+++ marking_and_admin/marking_puller.py
@@ -10,21 +10,22 @@ from mark_functions import do_the_marking
 # MARKING_SPREADSHEET_ID = "1wtTAM7A--ka7Lnog43L6jjo9kMCnDElCrTOBllEg4dA"  # 2019
 # MARKING_SPREADSHEET_ID = "1AjDu51VX26bIcLNMsr2iHq2BtrNEj91krxWKqjDW5aA"  # 2020
 # MARKING_SPREADSHEET_ID = "17KKMNIseRSo9IVNp-iaUCyEqbAR9tTYAcegzcvVgJFM"  # 2021
-MARKING_SPREADSHEET_ID = "16tESt_4BUf-9-oD04suTprkd1O0oEl6WjzflF_avSKY"  # 2022
+# MARKING_SPREADSHEET_ID = "16tESt_4BUf-9-oD04suTprkd1O0oEl6WjzflF_avSKY"  # 2022
+MARKING_SPREADSHEET_ID = "1DPBVy9DiVkdFBArOTRtj3L--f62KTnxyFFZrUXrobV0"  # 2023
 
 
 if __name__ == "__main__":
     do_the_marking(
-        this_year="2022",
+        this_year="2023",
         rootdir="../StudentRepos",
         chatty=False,
         force_marking=False,
         marking_spreadsheet_id=MARKING_SPREADSHEET_ID,
         marks_csv="marks.csv",
         mark_w1=True,
-        mark_w2=True,
-        mark_w3=True,
-        mark_w4=True,
+        mark_w2=False,
+        mark_w3=False,
+        mark_w4=False,
         mark_w5=False,
         mark_exam=False,
     )
