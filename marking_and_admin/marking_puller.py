1d456d8f895b67b4485ab1d32a550bc90ac3eda0
Ben <ben@notionparallax.co.uk>
2023-06-24 05:44:37 +0000

    ove spreadsheet ID to the secret manager

--- marking_and_admin/marking_puller.py
+++ marking_and_admin/marking_puller.py
@@ -4,6 +4,8 @@
 This pulls the latest copy of all the repos
 It can clone new repos if you delete the students pickle
 """
+import os
+
 from mark_functions import do_the_marking
 
 # The ID and range of a sample spreadsheet.
@@ -11,7 +13,8 @@ from mark_functions import do_the_marking
 # MARKING_SPREADSHEET_ID = "1AjDu51VX26bIcLNMsr2iHq2BtrNEj91krxWKqjDW5aA"  # 2020
 # MARKING_SPREADSHEET_ID = "17KKMNIseRSo9IVNp-iaUCyEqbAR9tTYAcegzcvVgJFM"  # 2021
 # MARKING_SPREADSHEET_ID = "16tESt_4BUf-9-oD04suTprkd1O0oEl6WjzflF_avSKY"  # 2022
-MARKING_SPREADSHEET_ID = "1DPBVy9DiVkdFBArOTRtj3L--f62KTnxyFFZrUXrobV0"  # 2023
+# MARKING_SPREADSHEET_ID = "1DPBVy9DiVkdFBArOTRtj3L--f62KTnxyFFZrUXrobV0"  # 2023
+MARKING_SPREADSHEET_ID = os.getenv("GOOGLE_SHEETS_KEY", "")
 
 
 if __name__ == "__main__":
