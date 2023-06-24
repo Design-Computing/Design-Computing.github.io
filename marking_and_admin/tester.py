9870d84eec64a4a7b2ffc42d13b379fd27ab8a06
Ben <ben@notionparallax.co.uk>
2023-06-24 05:49:34 +0000

    convert to using with

--- marking_and_admin/tester.py
+++ marking_and_admin/tester.py
@@ -1,8 +1,9 @@
 # -*- coding: UTF-8 -*-
 """Run a lightweight test set on specific students."""
-from marking_puller import RunCmd
 import os
 import time
+
+from marking_puller import RunCmd
 
 LOCAL = os.path.dirname(os.path.realpath(__file__))
 week_number = 1
@@ -25,10 +26,9 @@ for name in repos:
     start_time = time.time()
     try:
         RunCmd(args, timeout_cap).Run()
+        with open(os.path.abspath("temp_results.json"), "r", encoding="utf-8") as trf:
+            results.append(trf.read())
 
-        temp_results = open(os.path.abspath("temp_results.json"), "r")
-        results.append(temp_results.read())
-        temp_results.close()
     except Exception as e:
         print(e)
     elapsed_time = time.time() - start_time
