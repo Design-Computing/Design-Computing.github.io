d2ad2535370bde90d6b89cc14a1215dd3c11e837
Ben Doherty <ben@notionparallax.co.uk>
2026-03-04 17:02:44 +1100

    move to marking

--- marking_and_admin/all week's tests.py
+++
@@ -1,4 +0,0 @@
-import os
-
-for i in range(1, 10):
-    os.system("python ../course/week{weekNumber}/tests.py".format(weekNumber=i))
--- marking_and_admin/lambdaTester.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-url = "https://56wbk1pp93.execute-api.us-west-2.amazonaws.com/test/anotherTry"
-
-data = {"dkey": "dval"}
-r = requests.get(url, json=data)
-print(r, r.json())
